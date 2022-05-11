afbf7c216dce36d92f3c35d8f88f7fcdb0f29ee6
anuj <anujoza3@gmail.com>
2022-05-10 21:53:00 -0500

    rxjs operators usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { tweets as tweet$ } from "./services/dataSource"
+import {
+  map,
+  timer,
+  timestamp,
+  of,
+  mergeMap,
+  interval,
+  filter,
+  ConnectableObservable
+} from "rxjs"
+
+interface Tweet {
+  account: string
+  timestamp: number
+  content: string
+}
 
 function App() {
+  const [tweets, setTweets] = useState<null | Tweet[]>(null)
+  const arr = tweet$.pipe(map((val: Tweet) => [val]))
+  const subscriptionRef: any = useRef(null)
+  const obser = of(tweets).pipe(
+    mergeMap((tweet) =>
+      timer(0, 5000).pipe(
+        timestamp(),
+        map((val) => {
+          return { tweet, val }
+        })
+      )
+    )
+  )
+  const newObs = of(tweets).pipe(
+    mergeMap((tweets) =>
+      interval(1000).pipe(
+        timestamp(),
+        map((timestampObj) => {
+          const filteredTweets = tweets?.filter(
+            (tweet) => timestampObj.timestamp - tweet.timestamp < 30000
+          )
+          return filteredTweets
+        })
+        // filter(val => ) // may be can use filter and do some chaining ?
+      )
+    )
+  )
+  // useEffect(() => {
+  //   subscriptionRef.current = tweet$.subscribe((tweet) => {
+  //     setTweets((tweets) => {
+  //       if (Array.isArray(tweets)) {
+  //         const newTweets = [tweet, ...tweets]
+  //         return newTweets
+  //       }
+  //       return [tweet]
+  //     })
+  //   })
+  //   return () => subscriptionRef.current.unsubscribe()
+  // }, [])
+
+  useEffect(() => {
+    subscriptionRef.current = tweet$
+      .pipe(map((val: Tweet) => [val]))
+      .subscribe((tweet) => {
+        setTweets((tweets) => {
+          if (Array.isArray(tweets)) {
+            return [...tweet, ...tweets]
+          }
+          return tweet
+        })
+      })
+    return () => subscriptionRef.current.unsubscribe()
+  }, [])
+
+  useEffect(() => {
+    const sub = newObs.subscribe((data) => console.log(data))
+    return () => sub.unsubscribe()
+  }, [newObs])
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   subscriptionRef.current.unsubscribe()
+    // }, 10000)
+  }, [])
+
+  // console.log(tweets)
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +103,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
